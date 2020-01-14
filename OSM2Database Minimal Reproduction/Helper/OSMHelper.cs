9606dc75f014d7133f1ae48b2ecf927c01a8d6e2
Sascha Pape <sascha.pape@netaachen.com>
2020-01-14 16:47:33 +0100

    Rewrite to directly access and process the osm.pbf File. (Huge Performance Increase)

---
+++ OSM2Database Minimal Reproduction/Helper/OSMHelper.cs
@@ -0,0 +1,234 @@
+﻿using osm2mssql.Library.Protobuf;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace DLAddresses.Helper
+{
+    public class OpenStreetMapsHelper
+    {
+        public const int MaxDataBlockSize = 32 * 1024 * 1024;
+        public const int MaxHeaderBlockSize = 64 * 1024;
+
+        public string downloadData_fromOpenstreetmaps()
+        {
+            string NordrheinWestfalen = "http://download.geofabrik.de/europe/germany/nordrhein-westfalen-latest.osm.pbf";
+            string Liechtenstein = "https://download.geofabrik.de/europe/liechtenstein-latest.osm.pbf"; // Zum Testen
+            string RegierungsbezierkKoeln = "http://download.geofabrik.de/europe/germany/nordrhein-westfalen/koeln-regbez-latest.osm.pbf";
+
+            var FileDownloadPath = Path.GetTempPath() + @"\Openstreetmap_Data.osm.pbf";
+
+            if (File.Exists(FileDownloadPath))
+            {
+                File.Delete(FileDownloadPath);
+            }
+
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(Liechtenstein, FileDownloadPath);
+            }
+
+            return FileDownloadPath;
+        }
+
+        public IEnumerable<Address> ReadAddresses(string fileName)
+        {
+            //var max_streetLength = 0;
+            //var max_housenumberLength = 0;
+            //var max_cityLength = 0;
+            //var max_postcodeLength = 0;
+            //var max_countryLength = 0;
+
+            using (var file = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                BlobHeader blobHeader = null;
+
+                while ((blobHeader = ReadBlobHeader(file)) != null)
+                {
+                    var block = ReadBlob(file, blobHeader) as PrimitiveBlock;
+                    if (block != null)
+                    {
+                        foreach (PrimitiveGroup group in block.PrimitiveGroup)
+                        {
+                            if (group.Ways == null)
+                                continue;
+                            foreach (var way in group.Ways)
+                            {
+                                var returnAddress = new Address() { Country = "DE" };
+                                bool anyAddress = false;
+
+                                if (way.Keys != null)
+                                {
+                                    for (int i = 0; i < way.Keys.Count; i++)
+                                    {
+                                        string tagType = block.StringTable[way.Keys[i]];
+                                        string tagValue = block.StringTable[way.Values[i]];
+
+                                        //int tempCount = tagValue.Length;
+
+                                        switch (tagType)
+                                        {
+                                            case "":
+                                                break;
+
+                                            case "addr:street":
+                                                anyAddress = true;
+                                                returnAddress.Street = tagValue;
+
+                                                //max_streetLength = tempCount > max_streetLength ? tempCount : max_streetLength;
+                                                break;
+                                            case "addr:housenumber":
+                                                anyAddress = true;
+                                                returnAddress.Housenumber = tagValue;
+
+                                                //max_housenumberLength = tempCount > max_housenumberLength ? tempCount : max_housenumberLength;
+                                                break;
+                                            case "addr:city":
+                                                anyAddress = true;
+                                                returnAddress.City = tagValue;
+
+                                                //max_cityLength = tempCount > max_cityLength ? tempCount : max_cityLength;
+                                                break;
+                                            case "addr:postcode":
+                                                anyAddress = true;
+                                                returnAddress.Postcode = tagValue;
+
+                                                //max_postcodeLength = tempCount > max_postcodeLength ? tempCount : max_postcodeLength;
+                                                break;
+                                            //case "addr:country":
+                                            //    anyAddress = true;
+                                            //    returnAddress.Country = tagValue;
+
+                                            //    //max_countryLength = tempCount > max_countryLength ? tempCount : max_countryLength;
+                                            //    break;
+                                            default:
+                                                break;
+                                        }
+                                    }
+                                }
+
+                                if (!anyAddress || string.IsNullOrEmpty(returnAddress.Street) || string.IsNullOrEmpty(returnAddress.Postcode) || string.IsNullOrEmpty(returnAddress.City))
+                                {
+                                    continue;
+                                }
+
+
+                                yield return returnAddress;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public DataTable convertAdressList_toDataTable(IEnumerable<Address> adresses)
+        {
+            DataTable dataTable = new DataTable();
+
+            if (adresses.Any() == false)
+            {
+                return dataTable;
+            }
+
+            dataTable.Columns.Add("Id");
+            dataTable.Columns.Add("Street");
+            dataTable.Columns.Add("Housenumber");
+            dataTable.Columns.Add("City");
+            dataTable.Columns.Add("Postcode");
+            dataTable.Columns.Add("Country");
+
+            using (FastMember.ObjectReader reader = FastMember.ObjectReader.Create(adresses, "Id", "Street", "Housenumber", "City", "Postcode", "Country"))
+            {
+                dataTable.Load(reader);
+            }
+
+            return dataTable;
+        }
+
+        private BlobHeader ReadBlobHeader(Stream inputStream)
+        {
+            if (inputStream.Position < inputStream.Length)
+            {
+                return Serializer.DeserializeWithLengthPrefix<BlobHeader>(inputStream, PrefixStyle.Fixed32BigEndian);
+            }
+            return null;
+        }
+
+        private object ReadBlob(Stream inputStream, BlobHeader header)
+        {
+            var buffer = new byte[header.DataSize];
+            inputStream.Read(buffer, 0, header.DataSize);
+            Blob blob;
+            using (var s = new MemoryStream(buffer))
+            {
+                blob = Serializer.Deserialize<Blob>(s);
+            }
+
+            Stream blobContentStream = null;
+            try
+            {
+                if (blob.Raw != null)
+                {
+                    blobContentStream = new MemoryStream(blob.Raw);
+                }
+                else if (blob.ZlibData != null)
+                {
+                    var deflateStreamData = new MemoryStream(blob.ZlibData);
+                    //skip ZLIB header
+                    deflateStreamData.Seek(2, SeekOrigin.Begin);
+                    blobContentStream = new DeflateStream(deflateStreamData, CompressionMode.Decompress);
+                }
+
+                if (header.Type.Equals("OSMData", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    if ((blob.RawSize.HasValue && blob.RawSize > MaxDataBlockSize) ||
+                        (blob.RawSize.HasValue == false && blobContentStream.Length > MaxDataBlockSize))
+                    {
+                        throw new InvalidDataException("Invalid OSMData block");
+                    }
+
+                    return Serializer.Deserialize<PrimitiveBlock>(blobContentStream);
+                }
+                else if (header.Type.Equals("OSMHeader", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    if ((blob.RawSize.HasValue && blob.RawSize > MaxHeaderBlockSize) ||
+                        (blob.RawSize.HasValue == false && blobContentStream.Length > MaxHeaderBlockSize))
+                    {
+                        throw new InvalidDataException("Invalid OSMHeader block");
+                    }
+
+                    return Serializer.Deserialize<OsmHeader>(blobContentStream);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            finally
+            {
+                if (blobContentStream != null)
+                {
+                    blobContentStream.Close();
+                    blobContentStream.Dispose();
+                    blobContentStream = null;
+                }
+            }
+        }
+    }
+
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string Housenumber { get; set; }
+        public string City { get; set; }
+        public string Postcode { get; set; }
+        public string Country { get; set; }
+    }
+}
--- OSM2Database Minimal Reproduction/Program.cs
+++ OSM2Database Minimal Reproduction/Program.cs
@@ -1,5 +1,4 @@
-﻿using OsmSharp;
-using OsmSharp.Streams;
+﻿using DLAddresses.Helper;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -12,105 +11,17 @@ namespace OSM2Database_Minimal_Reproduction
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            string FileDownloadPath = DownloadOSMData();
-            DataTable dataTable = getDataTable_fromOSMFile(FileDownloadPath);
-            importDataTable_toDatabase(dataTable);
+            OpenStreetMapsHelper OsmHelper = new OpenStreetMapsHelper();
 
-            Console.ReadKey();
-        }
+            string FileDownloadPath = OsmHelper.downloadData_fromOpenstreetmaps();
 
-        public static string DownloadOSMData()
-        {
-            Console.WriteLine("Trying to get Data from OpenStreetMaps...");
+            IEnumerable<Address> addresses = OsmHelper.ReadAddresses(FileDownloadPath);
 
-            string NordrheinWestfalen = "http://download.geofabrik.de/europe/germany/nordrhein-westfalen-latest.osm.pbf";
-            string Liechtenstein = "https://download.geofabrik.de/europe/liechtenstein-latest.osm.pbf"; // testing
-            string RegierungsbezierkKoeln = "http://download.geofabrik.de/europe/germany/nordrhein-westfalen/koeln-regbez-latest.osm.pbf";
+            DataTable dataTable = OsmHelper.convertAdressList_toDataTable(addresses);
 
-            var FileDownloadPath = Path.GetTempPath() + @"\Openstreetmap_Data.osm.pbf";
-
-            if (File.Exists(FileDownloadPath))
-            {
-                File.Delete(FileDownloadPath);
-            }
-
-            using (var client = new WebClient())
-            {
-                client.DownloadFile(RegierungsbezierkKoeln, FileDownloadPath);
-            }
-
-            return FileDownloadPath;
-        }
-
-        public static DataTable getDataTable_fromOSMFile(string FileDownloadPath)
-        {
-
-            Console.WriteLine("Finished Downloading. Reading File into Stream...");
-
-            using (var fileStream = new FileInfo(FileDownloadPath).OpenRead())
-            {
-                PBFOsmStreamSource source = new PBFOsmStreamSource(fileStream);
-
-                if (source.Any() == false)
-                {
-                    return new DataTable();
-                }
-
-                Console.WriteLine("Finished Reading File into Stream. Filtering and Formatting RawData to Addresses...");
-                Console.WriteLine();
-
-                DataTable dataTable = convertAdressList_toDataTable(
-                    source.Where(x => x.Type == OsmGeoType.Way && x.Tags.Count > 0 && x.Tags.ContainsKey("addr:street"))
-                            .Select(Address.fromOSMGeo)
-                            .Distinct(new AddressComparer())
-                );
-
-                return dataTable;
-            }
-        }
-
-        private static DataTable convertAdressList_toDataTable(IEnumerable<Address> adresses)
-        {
-            DataTable dataTable = new DataTable();
-
-            if (adresses.Any() == false)
-            {
-                return dataTable;
-            }
-
-            dataTable.Columns.Add("Id");
-            dataTable.Columns.Add("Street");
-            dataTable.Columns.Add("Housenumber");
-            dataTable.Columns.Add("City");
-            dataTable.Columns.Add("Postcode");
-            dataTable.Columns.Add("Country");
-
-            Int32 counter = 0;
-
-            Console.WriteLine("Finished Filtering and Formatting. Writing Addresses From Stream to a DataTable Class for the Database-SQLBulkCopy-Process ");
-
-            foreach (Address address in adresses)
-            {
-                dataTable.Rows.Add(counter + 1, address.Street, address.Housenumber, address.City, address.Postcode, address.Country);
-                counter++;
-
-                if (counter % 10000 == 0 && counter != 0)
-                {
-                    Console.WriteLine("Wrote " + counter + " Rows From Stream to DataTable.");
-                }
-            }
-
-            return dataTable;
-        }
-
-        public static void importDataTable_toDatabase(DataTable dataTable)
-        {
-            string connectionString = "Look at me. Im a Connectionstring";
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection("I am a Connectionstring"))
             {
                 connection.Open();
                 SqlTransaction transaction = connection.BeginTransaction();
@@ -124,112 +35,28 @@ namespace OSM2Database_Minimal_Reproduction
                     command.CommandText = "TRUNCATE TABLE Addresses";
                     command.ExecuteNonQuery();
 
-                    using (SqlBulkCopy sqlBulk = new SqlBulkCopy(connection, SqlBulkCopyOptions.KeepIdentity, transaction))
+                    using (var sql_bulk_copy = new SqlBulkCopy(connection, SqlBulkCopyOptions.Default, transaction))
                     {
-                        sqlBulk.NotifyAfter = 10000;
-                        sqlBulk.SqlRowsCopied += (sender, eventArgs) => Console.WriteLine("Wrote " + eventArgs.RowsCopied + " records...");
-                        sqlBulk.DestinationTableName = "Addresses";
-                        sqlBulk.WriteToServer(dataTable);
+                        //sql_bulk_copy.NotifyAfter = 10000;
+                        //sql_bulk_copy.SqlRowsCopied += (sender, eventArgs) => Helper.OpenStreetMapsHelper.logToFile("Wrote " + eventArgs.RowsCopied + " records...");
+                        sql_bulk_copy.BatchSize = 10000;
+                        sql_bulk_copy.DestinationTableName = "Addresses";
+                        sql_bulk_copy.WriteToServer(dataTable);
                     }
 
                     transaction.Commit();
                 }
-                catch
+                catch (Exception ex1)
                 {
-                    Console.WriteLine("Something went wront. Trying Rollback now...");
-
                     try
                     {
                         transaction.Rollback();
-
-                        Console.WriteLine("Rollback completed...");
-                        return;
                     }
                     catch (Exception ex2)
                     {
-                        Console.WriteLine("Error during Rollback. Rollback could not be completed...");
-                        return;
                     }
                 }
             }
-
-            Console.WriteLine("Import successful");
-
-            return;
-        }
-    }
-
-    public class Address
-    {
-        public int Id { get; set; }
-
-        public string Street { get; set; }
-        public string Housenumber { get; set; }
-        public string City { get; set; }
-        public string Postcode { get; set; }
-        public string Country { get; set; }
-
-        public static Func<OsmGeo, Address> fromOSMGeo = x =>
-        {
-            var result = new Address();
-
-            if (x != null && x.Tags != null && x.Tags.Count > 0)
-            {
-                x.Tags.TryGetValue("addr:street", out string street);
-                x.Tags.TryGetValue("addr:housenumber", out string housenumber);
-                x.Tags.TryGetValue("addr:city", out string city);
-                x.Tags.TryGetValue("addr:postcode", out string postcode);
-                x.Tags.TryGetValue("addr:country", out string country);
-
-                result.Street = street;
-                result.Housenumber = housenumber;
-                result.City = city;
-                result.Postcode = postcode;
-                result.Country = country;
-            };
-
-            return result;
-        };
-    }
-
-    public class AddressComparer : IEqualityComparer<Address>
-    {
-        // Wird benötigt für Except Methodenaufrufe die Objekte nicht nur nach der ID vergleichen sollen
-        // ... sondern auch nach allen anderen Eigenschaften
-
-        public bool Equals(Address x, Address y)
-        {
-            if (x.Street != y.Street)
-            {
-                return false;
-            }
-
-            if (x.Housenumber != y.Housenumber)
-            {
-                return false;
-            }
-
-            if (x.City != y.City)
-            {
-                return false;
-            }
-
-            if (x.Postcode != y.Postcode)
-            {
-                return false;
-            }
-
-            if (x.Country != y.Country)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        public int GetHashCode(Address address)
-        {
-            return address.Id.GetHashCode();
         }
     }
 }
